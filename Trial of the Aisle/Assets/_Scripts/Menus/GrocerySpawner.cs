414f964ff3fc050efcc735de22c3eb71454e58a9
Lucas Z <Lucas.Zakaria03@gmail.com>
2024-05-19 15:03:50 -0400

    Updated Main Menu
    Now we have a pretty cool main menu with satisfying buttons and a showcase of the grocery theme

---
+++ Trial of the Aisle/Assets/_Scripts/Menus/GrocerySpawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrocerySpawner : MonoBehaviour
+{
+    [SerializeField] private float timeForSpawn;
+    [SerializeField] private GameObject[] objectsToSpawn;
+
+    private IEnumerator spawnGroceryCoroutine;
+
+    void Start()
+    {
+        spawnGroceryCoroutine = SpawnGroceryCorouitne();
+        StartCoroutine(spawnGroceryCoroutine);
+    }
+
+    private IEnumerator SpawnGroceryCorouitne()
+    {
+        while(true)
+        {
+            Instantiate(objectsToSpawn[Random.Range(0,objectsToSpawn.Length)], transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(timeForSpawn);
+        }
+        
+    }
+   
+}
