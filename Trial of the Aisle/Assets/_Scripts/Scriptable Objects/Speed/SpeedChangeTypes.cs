98c42479787798b7cb79326d011e166779d707ae
cerohaven <amckenzie09@hotmail.com>
2024-05-26 13:24:07 -0400

    Post Merge Conflict
    ????

---
+++ Trial of the Aisle/Assets/_Scripts/Scriptable Objects/Speed/SpeedChangeTypes.cs
@@ -0,0 +1,15 @@
+//The//These Speed adjustments will apply to both buffs and debuffs
+public enum ChangeSpeed : byte
+{
+    X_Small_Speed,
+    Small_Speed,
+    Medium_Speed,
+    Large_Speed,
+    X_Large_Speed,
+}
+
+public enum SpeedType : byte
+{
+    Buff,
+    Debuff
+}
