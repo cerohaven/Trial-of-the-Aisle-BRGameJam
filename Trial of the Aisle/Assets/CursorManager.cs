eef8807269dc6521e6414430c76a76e5e2a20ba0
cerohaven <amckenzie09@hotmail.com>
2024-06-16 19:15:15 -0400

    Fixed Heirarchy bug
    Player no longer destroyed between scenes, Fixed Cursor Delay between scenes, Found new bug

--- Trial of the Aisle/Assets/CursorManager.cs
+++ Trial of the Aisle/Assets/CursorManager.cs
@@ -1,9 +1,26 @@
 using UnityEngine;
+using System.Collections;
 
 public class CursorManager : MonoBehaviour
 {
     public Texture2D defaultCursor;
     public Texture2D reticleCursor;
+    [SerializeField]private float WaitTime = 1f;
+
+    private static CursorManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void Start()
     {
@@ -17,6 +34,12 @@ public class CursorManager : MonoBehaviour
 
     public void SetReticleCursor()
     {
+        StartCoroutine(DelayedSetReticleCursor());
+    }
+
+    private IEnumerator DelayedSetReticleCursor()
+    {
+        yield return new WaitForSeconds(WaitTime);
         Cursor.SetCursor(reticleCursor, Vector2.zero, CursorMode.Auto);
     }
 }
