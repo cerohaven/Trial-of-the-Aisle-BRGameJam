42e81d77f326532a74af84080a3d9990bcecf2b6
Lucas Z <Lucas.Zakaria03@gmail.com>
2024-06-21 01:36:19 -0400

    Fixed Tooltip Variables and Added Employee Images
    Now the tooltip cannot go past the bounds of the screen. I did this with hard-coded values since non-hardcoded values scale with the screenSize and is different for all sizes of screens
    
    - Also imported the employee drawings of us that Kien made and put them in the Abount Scene!

--- Trial of the Aisle/Assets/_Scripts/NewAbilitySystem/UI/NewAbilitySelectionUI.cs
+++ Trial of the Aisle/Assets/_Scripts/NewAbilitySystem/UI/NewAbilitySelectionUI.cs
@@ -80,6 +80,7 @@ public class NewAbilitySelectionUI : MonoBehaviour
 
         
     }
+
     private void MouseHoverOverAbility(GameObject textPanel)
     {
         textPanel.SetActive(true);
@@ -102,21 +103,8 @@ public class NewAbilitySelectionUI : MonoBehaviour
 
     void Update()
     {
-        //making sure the text panels stay within the bounds of the screen
-        if (abilityOneTextPanel != null)
-        {
-            abilityTextPanelRects[0].anchoredPosition = Vector2.zero;
-            abilityTextPanelRects[0].anchoredPosition -= KeepFullyOnScreen(abilityTextPanelRects[0], abilityOneTextPanel.transform.position);
-            //KeepFullyOnScreenFlip(abilityTextPanelRects[0], abilityOneTextPanel.transform.position);
-        }
-        if(abilityTwoTextPanel != null)
-        {
-            abilityTextPanelRects[1].anchoredPosition = Vector2.zero;
-            abilityTextPanelRects[1].anchoredPosition -= KeepFullyOnScreen(abilityTextPanelRects[1], abilityTwoTextPanel.transform.position);
-            //KeepFullyOnScreenFlip(abilityTextPanelRects[1], abilityTwoTextPanel.transform.position);
-        }
-        
-
+        Debug.DrawLine(new Vector3(Screen.width / 2.5f, 0, 0), new Vector3(Screen.width / 2.5f, 1280, 0));
+        Debug.DrawLine(new Vector3(0, Screen.height / 1.3f, 0), new Vector3(1920, Screen.height / 1.3f, 0));
         if (Input.GetKeyDown(KeyCode.Escape) && unlockPanel.activeSelf)
         {
             unlockPanel.SetActive(false);
@@ -124,53 +112,5 @@ public class NewAbilitySelectionUI : MonoBehaviour
         }
     }
 
-    //https://forum.unity.com/threads/keep-ui-objects-inside-screen.523766/
-    //Only Offsets as much is needed to not go off screen
-    Vector2 KeepFullyOnScreen(RectTransform panel, Vector3 newPos)
-    {
-        offset = Vector2.zero;
-
-        float maxX = (CanvasRect.sizeDelta.x - panel.sizeDelta.x - blackBorderWidth * 2) * 0.5f;
-        float maxY = (CanvasRect.sizeDelta.y - panel.sizeDelta.y - blackBorderWidth) * 0.5f;
-
-        //offset the current local position based on how much we're off screen
-        //If we don't add this code, the text box's position will stay in the corrected spot forever
-        if (newPos.x > maxX)
-        {
-            offset.x = newPos.x - maxX;
-        }
-        if(newPos.y > maxY)
-        {
-            offset.y = newPos.y - maxY;
-        }
-        
-        return offset;
-    }
-
-    //Flips the entire image and adjusts the anchor position
-    private void KeepFullyOnScreenFlip(RectTransform panel, Vector3 newPos)
-    {
-
-        float maxX = (CanvasRect.sizeDelta.x - panel.sizeDelta.x) * 0.5f;
-        float maxY = (CanvasRect.sizeDelta.y - panel.sizeDelta.y) * 0.5f;
-
-        //offset the current local position based on how much we're off screen
-        //If we don't add this code, the text box's position will stay in the corrected spot forever
-
-        bool pastScreenX = newPos.x > maxX;
-        bool pastScreenY = newPos.y > maxY;
-
-        if (pastScreenX)
-        {
-            panel.anchoredPosition = new Vector2(-panel.sizeDelta.x + 130, panel.anchoredPosition.y);
-        }
-
-        if (pastScreenY)
-        {
-            panel.anchoredPosition = new Vector2(panel.anchoredPosition.x, -panel.sizeDelta.y + 80);
-        }
-
-
-    }
-
+  
 }
