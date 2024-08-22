1146015935134bb83ca900038b5b8d51eb6e2993
Lucas Z <Lucas.Zakaria03@gmail.com>
2024-08-22 05:47:10 -0400

    Finished Projectile Patterns Types!
    Now all the types are implemented and stack onto each other. We can create really interesting designs using this system

--- Trial of the Aisle/Assets/_Scripts/Projectile Patterns/SO_ProjectilePattern.cs
+++ Trial of the Aisle/Assets/_Scripts/Projectile Patterns/SO_ProjectilePattern.cs
@@ -1,8 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
-
 
 
 [System.Serializable]
@@ -25,7 +22,7 @@ public class SO_ProjectilePattern : ScriptableObject
     [SerializeField] private List<ProjectilePattern> _projectilePatterns = new List<ProjectilePattern>();
 
     [SerializeField] private List<bool> _foldouts = new List<bool>();
-    [SerializeField] private bool _baseFoldout;
+    [SerializeField] private bool _baseFoldout = true;
     [SerializeField] private int _removeSpecificPattern;
     //These Structs contain all the logic we need to store for each parameter/modifier in each pattern type.
 
@@ -58,7 +55,11 @@ public class SO_ProjectilePattern : ScriptableObject
     {
         new PatternTypeMod("Number of Projectiles", 1),     //0
         new PatternTypeMod("Spread Angle", 1),              //1
-        new PatternTypeMod("Delay Between Projectiles", 0)  //2
+        new PatternTypeMod("Spread Speed Increase", 0f),    //2
+        new PatternTypeMod("Delay Between Projectiles", 0), //3
+        new PatternTypeMod("Mirror", 0),                    //4
+        new PatternTypeMod("Shift", 0),                     //5
+        new PatternTypeMod("Center Remove Number", 0)       //6
     };
 
     [SerializeField]
@@ -121,18 +122,59 @@ public class SO_ProjectilePattern : ScriptableObject
                     break;
 
                 case ProjectilePatterns.Spread:
-
+                    int amountOfProjectiles = (int)_projectilePatterns[i].thisPatternTypeModifiers[0].modValue;
+                    float extraAngle = _projectilePatterns[i].thisPatternTypeModifiers[1].modValue;
+                    float speed = _projectilePatterns[i].thisPatternTypeModifiers[2].modValue;
+                    float delay = _projectilePatterns[i].thisPatternTypeModifiers[3].modValue;
+                    bool mirror = _projectilePatterns[i].thisPatternTypeModifiers[4].modValue > 0;
+                    bool shift = _projectilePatterns[i].thisPatternTypeModifiers[5].modValue > 0;
+                    int spreadRemoveNumber = (int)_projectilePatterns[i].thisPatternTypeModifiers[6].modValue;
+                    bool isEven = _projectilePatterns[i].thisPatternTypeModifiers[0].modValue % 2 == 0;
+                    float sum = extraAngle * (amountOfProjectiles-1);
+                    //Loop through all the previous projectiles
                     for (int j = 0; j < tempProjectileList.Count; j++)
                     {
-                        for (int k = 1; k < (int)_projectilePatterns[i].thisPatternTypeModifiers[0].modValue; k++)
+                        //Loop the amount of projectiles we want to spawn for the spread
+                        for (int k = 0 + spreadRemoveNumber; k < amountOfProjectiles; k++)
                         {
+                            
+                            float shiftAmount = 0;
+
+
+                            if(shift)
+                            {
+                                if(!isEven)
+                                {
+                                    shiftAmount = extraAngle * Mathf.Floor(amountOfProjectiles / 2);
+                                }
+                                else
+                                {
+
+                                    shiftAmount = (sum / amountOfProjectiles) * (amountOfProjectiles / 2.00f);
+                                }
+                            }
+
                             temptempProjectileList.Add(CreateNewProjectile(basePAT[0].modValue, //Angle
-                                                                           tempProjectileList[j][1].modValue, //Speed
-                                                                           tempProjectileList[j][2].modValue +
-                                                                           _projectilePatterns[i].thisPatternTypeModifiers[1].modValue * k, //Extra Angle
-                                                                           tempProjectileList[j][3].modValue +
-                                                                           _projectilePatterns[i].thisPatternTypeModifiers[2].modValue * k)); //Delay
+                                                                           tempProjectileList[j][1].modValue + speed * k,                   //Speed
+                                                                           tempProjectileList[j][2].modValue + extraAngle * k - shiftAmount, //Extra Angle
+                                                                           tempProjectileList[j][3].modValue + delay * k));                 //Delay
+                            
+                            //If we're mirroring, we create a new bullet on the other side
+                            if(mirror)
+                            {
+                                temptempProjectileList.Add(CreateNewProjectile(basePAT[0].modValue, //Angle
+                                                                           tempProjectileList[j][1].modValue + speed * k,           //Speed
+                                                                           tempProjectileList[j][2].modValue - extraAngle * k,      //Extra Angle
+                                                                           tempProjectileList[j][3].modValue + delay * k));         //Delay
+                            }
+
                         }
+
+                        if (mirror || shift)
+                        {
+                            tempProjectileList.RemoveAt(i);
+                        }
+
                     }
                     break;
 
@@ -145,7 +187,7 @@ public class SO_ProjectilePattern : ScriptableObject
                         float range = _projectilePatterns[i].thisPatternTypeModifiers[0].modValue;
                         float randomAngle = Random.Range(-range, range);
 
-                        tempProjectileList[j][2].modValue = randomAngle;
+                        tempProjectileList[j][2].modValue += randomAngle;
 
                     }
 
@@ -159,7 +201,7 @@ public class SO_ProjectilePattern : ScriptableObject
                         {
                             Debug.Log(k);
                             temptempProjectileList.Add(CreateNewProjectile(basePAT[0].modValue,
-                                                                           basePAT[1].modValue,
+                                                                           tempProjectileList[j][1].modValue,
                                                                            tempProjectileList[j][2].modValue,
                                                                            tempProjectileList[j][3].modValue +
                                                                            _projectilePatterns[i].thisPatternTypeModifiers[1].modValue * k));
