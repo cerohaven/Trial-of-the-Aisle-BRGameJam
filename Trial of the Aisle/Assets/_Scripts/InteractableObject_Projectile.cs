59f79a8797f45cbd1ec2ad459e3d8051e11ae956
Keanichii <hopha@sheridancollege.ca>
2024-08-24 01:17:01 -0400

    Reapply "Merge remote-tracking branch 'origin/ProjectilePatterns' into Drag-Drop-Mechanic"
    This reverts commit 05276a6cd18bbceea18f9bb27cc7621436a10e80.

--- Trial of the Aisle/Assets/_Scripts/InteractableObject_Projectile.cs
+++ Trial of the Aisle/Assets/_Scripts/InteractableObject_Projectile.cs
@@ -40,11 +40,6 @@ public class InteractableObject_Projectile : InteractableObject
         playerCarry.IsCarryingObject = false;
         playerCarry.SetActiveAimArrows(false);
 
-        //Now I need to re-enable collision with the boss layer
-        projectile.IgnoreBossCollision(false, bossCollider);
-
-        StartCoroutine(projectile.IgnoreProjectilesCoroutine(false, 0));
-
 
         RuntimeManager.PlayOneShot("event:/SFX/Bosses/General/ThrowProjectile");
         //AudioManager.instance.Play("p_throw");
---
+++ Trial of the Aisle/Assets/_Scripts/Projectile Patterns/TestProjectile.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestProjectile : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public void Initialize(Vector2 travelDir, float speed)
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = travelDir * speed;
+        Invoke(nameof(DestroyObj), 3);
+    }
+
+    void DestroyObj()
+    {
+        Destroy(gameObject);
+    }
+}
